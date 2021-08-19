eb8a03a5972b4678202789dbad3d8eb1bcf8edf4
Jasper Wieringa <jasper.wieringa@arup.com>
2021-08-19 15:04:38 +0200

    Added the Container web-component

---
+++ src/components/frame/Container.js
@@ -0,0 +1,31 @@
+import { LitElement, html, css } from 'lit';
+
+export class Container extends LitElement {
+  static get tag() {
+    return 'arc-container';
+  }
+
+  static get properties() {
+    return {};
+  }
+
+  static get styles() {
+    return css`
+      .main {
+        min-height: 100vh;
+      }
+    `;
+  }
+
+  render() {
+    return html`
+      <main class="main">
+        <slot id="nav" name="nav"></slot>
+        <slot id="side" name="side"></slot>
+        <slot id="content" name="content"></slot>
+      </main>
+    `;
+  }
+}
+
+customElements.define(Container.tag, Container);

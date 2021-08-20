02e29cf6fc50c2a2600b1dec5b8d102b539d70e4
Jasper Wieringa <Jasper.Wieringa@arup.com>
2021-08-20 10:50:13 +0200

    Added some mediaqueries using the correct em units

--- src/components/frame/Container.js
+++ src/components/frame/Container.js
@@ -11,20 +11,57 @@ export class Container extends LitElement {
 
   static get styles() {
     return css`
-      .main {
+      #main {
         min-height: 100vh;
         display: flex;
         flex-direction: column;
+        background: var(--arc-grey-010);
       }
+      #nav::slotted(arc-navbar) {
+        background: var(--arc-red-040);
+        height: 56px;
+      }
+      
+      /* Content */
+      #container {
+        flex: 1 1 100%;
+        display: flex;
+      }
+      ::slotted(arc-sidebar) {
+        background: var(--arc-blue-040);
+        width: 0;
+      }
+      ::slotted(arc-content) {
+        background: var(--arc-purple-040);
+        flex: 1 1 100%;
+      }
+      
+      /* Media Queries */
+      /* Small devices (landscape phones, 576px and up)  */
+      @media (min-width: 36em) { 
+        ::slotted(arc-sidebar) {
+          width: 368px;
+        }
+      }
+      /* Medium devices (tablets, 768px and up)  */
+      @media (min-width: 48em) { ... }
+      /* Large devices (desktops, 992px and up)  */
+      @media (min-width: 62em) { ... }
+      /* X-Large devices (large desktops, 1200px and up)  */
+      @media (min-width: 75em) { ... }
+      /* XX-Large devices (larger desktops, 1400px and up)  */
+      @media (min-width: 87.5em) { ... }
     `;
   }
 
   render() {
     return html`
-      <main class="main">
+      <main id="main">
         <slot id="nav" name="nav"></slot>
-        <slot id="side" name="side"></slot>
-        <slot id="content" name="content"></slot>
+        <div id="container">
+          <slot name="side"></slot>
+          <slot name="content"></slot>
+        </div>
       </main>
     `;
   }

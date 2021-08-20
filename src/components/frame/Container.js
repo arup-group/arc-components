5b68af6d8f2780ab3d89ecd877b7da6b1445ebea
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-08-20 17:42:35 +0200

    Added a static slots getter to show the available slots inside the Container web-component Updated the fontsize-normal to 1rem Added an ignore-path for the lint and format scripts to avoid formatting node_modules, coverage_reports etc.

--- src/components/frame/Container.js
+++ src/components/frame/Container.js
@@ -9,60 +9,73 @@ export class Container extends LitElement {
     return {};
   }
 
+  static get slots() {
+    return ['nav', 'side', '', 'bottom'];
+  }
+
   static get styles() {
     return css`
       :host {
         --navbar-height: 3.5rem;
-        --navbar-width: 23rem;
+        --sidebar-width: 23rem;
       }
-      
+
       #main {
         min-height: 100vh;
         display: flex;
         flex-direction: column;
         background: var(--arc-grey-010);
       }
-      
-      ::slotted(*) {
-        /* TODO: Use arc-white-000 once testing is done */
+
+      /* TODO: Set to arc-white-000 */
+      ::slotted(*),
+      #bottom {
         background: var(--arc-red-010);
       }
-      
-      /* Navbar */
-      ::slotted(arc-navbar) {
+
+      /* Navbar / Bottom */
+      ::slotted(arc-navbar),
+      ::slotted(arc-bottombar),
+      #bottom {
         height: var(--navbar-height);
       }
-      
+
       /* Content */
       #container {
         flex: 1 1 100%;
         display: flex;
         padding: var(--arc-spacing-medium);
       }
+
       ::slotted(arc-sidebar) {
-        width: 0;
-        transition: all var(--arc-transition-slow);
+        width: var(--sidebar-width);
+        margin-right: var(--arc-spacing-medium);
       }
+
       ::slotted(arc-content) {
         flex: 1 1 100%;
       }
-      
-      /* Media Queries */
-      /* Small devices (landscape phones, 576px and up)  */
-      @media (min-width: 36em) { 
+
+      ::slotted(arc-bottombar),
+      #bottom {
+        display: none;
+      }
+
+      /* Medium devices (tablets, 768px and up)  */
+      @media (max-width: 48em) {
+        #container {
+          padding: var(--arc-spacing-medium) 0 0 0;
+        }
+
         ::slotted(arc-sidebar) {
-          width: var(--navbar-width);
-          margin-right: var(--arc-spacing-medium);
+          display: none;
+        }
+
+        ::slotted(arc-bottombar),
+        #bottom {
+          display: block;
         }
       }
-      /* Medium devices (tablets, 768px and up)  */
-      @media (min-width: 48em) { ... }
-      /* Large devices (desktops, 992px and up)  */
-      @media (min-width: 62em) { ... }
-      /* X-Large devices (large desktops, 1200px and up)  */
-      @media (min-width: 75em) { ... }
-      /* XX-Large devices (larger desktops, 1400px and up)  */
-      @media (min-width: 87.5em) { ... }
     `;
   }
 
@@ -72,8 +85,11 @@ export class Container extends LitElement {
         <slot id="nav" name="nav"></slot>
         <div id="container">
           <slot name="side"></slot>
-          <slot name="content"></slot>
+          <slot></slot>
         </div>
+        <slot name="bottom">
+          <arc-bottombar id="bottom">DEFAULT BOTTOM BAR</arc-bottombar>
+        </slot>
       </main>
     `;
   }

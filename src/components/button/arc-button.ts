98825e2c39d125f0f0ce8ed45ebcc78d65673c2a
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-08 14:08:21 +0200

    Set the button width to be based on the content

--- src/components/button/arc-button.ts
+++ src/components/button/arc-button.ts
@@ -6,8 +6,8 @@ export class ArcButton extends LitElement {
   static styles = css`
     :host {
       display: inline-block;
+      cursor: pointer;
       width: auto;
-      --min-width: 7.5rem;
     }
 
     :host *, :host ::before, :host ::after {
@@ -16,72 +16,86 @@ export class ArcButton extends LitElement {
 
     #button {
       min-height: 100%;
-      min-width: var(--min-width);
       display: flex;
       align-items: center;
       justify-content: center;
-      font-family: var(--arc-button-font-family);
-      font-size: var(--arc-button-font-size);
-      font-weight: var(--arc-button-font-weight);
-      cursor: pointer;
+      font-family: var(--arc-font-button);
+      font-size: var(--arc-font-size-small);
+      font-weight: var(--arc-font-weight-semibold);
+      cursor: inherit;
       line-height: normal;
       outline: none;
-      padding: var(--arc-spacing-normal);
+      padding: 0;
       -webkit-appearance: none;
       white-space: nowrap;
       transition: var(--arc-transition-fast) background-color, var(--arc-transition-fast) color, var(--arc-transition-fast) border, var(--arc-transition-fast) box-shadow;
     }
 
+    /* Types */
     #button.normal {
-      border: none;
       color: var(--arc-button-color);
-    }
-
-    #button.primary {
-
-    }
-
-    #button.text {
-
-    }
-
-    #button.tile {
-
+      background: var(--arc-button-background-color);
+      border: none;
     }
 
     #button.tab {
       color: var(--arc-tab-color);
-      text-decoration: none;
-      user-select: none;
       background: none;
       border: none;
+      text-decoration: none;
+      user-select: none;
     }
 
-    span {
+    /* Sizes */
+    #button.small {
+      height: var(--arc-input-height-small);
+    }
+
+    #button.medium {
+      height: var(--arc-input-height-medium);
+    }
+
+    #button.large {
+      height: var(--arc-input-height-large);
+    }
+
+    #button.small > span {
+      padding: 0 var(--arc-spacing-small);
+    }
+
+    #button.medium > span {
       padding: 0 var(--arc-spacing-medium);
     }
 
+    #button.large > span {
+      padding: 0 var(--arc-spacing-large);
+    }
+
     /* Disabled */
-    :host([disabled]:not([disabled='false'])) #button {
+    :host([disabled]) #button {
       color: var(--arc-button-color-disabled);
       opacity: 0.5;
       cursor: not-allowed;
     }
 
     /* Active */
-    :host([active]:not([active='false'])[disabled='false']) #button.tab {
+    :host([active]:not([disabled])) #button.tab {
       border-bottom: solid 2px currentColor;
     }
 
     /* Hover */
-    :host(:not([disabled='true'])) #button:hover {
+    :host(:not([disabled])) #button:hover {
       background-color: var(--arc-button-color-hover);
     }
   `;
 
-  /** @type { 'normal' | 'primary' | 'text' | 'tile' | 'tab' } */
+  /** @type { 'normal' | 'tab' } */
   @property({type: String, reflect: true })
   type: string = 'normal';
+
+  /** @type { 'small' | 'medium' | 'large' } */
+  @property({type: String, reflect: true })
+  size: string = 'medium';
 
   @property({type: Boolean, reflect: true})
   rounded: boolean = false;
@@ -104,6 +118,9 @@ export class ArcButton extends LitElement {
   updated(changedProperties: PropertyValues<this>) {
     if (changedProperties.has('type')) {
       this.button.classList.add(this.type);
+    }
+    if (changedProperties.has('size')) {
+      this.button.classList.add(this.size);
     }
   }
 

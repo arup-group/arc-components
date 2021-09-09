7cea27912148f6e888e9723a233c13c2b0680615
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-09 13:29:11 +0200

    Fixed the styling / colors / hover-effects of the button web-component

--- src/components/button/arc-button.ts
+++ src/components/button/arc-button.ts
@@ -15,16 +15,17 @@ export class ArcButton extends LitElement {
       box-sizing: inherit;
     }
 
+    /* Main */
     #button {
       min-height: 100%;
       width: 100%;
       display: flex;
       align-items: center;
       justify-content: center;
-      color: var(--btn-color);
-      background-color: var(--btn-background-color);
-      border: var(--arc-border-width) solid var(--btn-border-color);
+      color: rgb(var(--arc-input-color));
+      border: none;
       border-radius: var(--arc-border-radius-medium);
+      box-shadow: var(--arc-box-shadow);
       font-family: var(--arc-font-button);
       font-size: var(--arc-font-size-small);
       font-weight: var(--arc-font-weight-semibold);
@@ -36,99 +37,80 @@ export class ArcButton extends LitElement {
       padding: 0;
       -webkit-appearance: none;
       white-space: nowrap;
-      transition: var(--arc-transition-fast) background-color, var(--arc-transition-fast) color, var(--arc-transition-fast) border, var(--arc-transition-fast) box-shadow;
     }
 
-    /* Contained */
-    :host([type='contained']) #button {
-      color: var(--arc-white-000);
-      border: none;
-      box-shadow: var(--arc-box-shadow);
-      background-color: var(--arc-color-default);
+    /* Main - Colors */
+    :host([color='default']) #button {
+      background-color: rgb(var(--arc-color-default));
     }
-    :host([type='contained'][color='default']) #button {
-      color: rgba(var(--arc-color-default), 1.5);
+    :host([color='primary']) #button {
+      color: rgb(var(--arc-background-color));
+      background-color: rgb(var(--arc-color-primary));
     }
-    :host([type='contained'][color='primary']) #button {
-      background-color: var(--arc-color-primary);
+    :host([color='secondary']) #button {
+      color: rgb(var(--arc-background-color));
+      background-color: rgb(var(--arc-color-secondary));
     }
-    :host([type='contained'][color='secondary']) #button {
-      background-color: var(--arc-color-secondary);
+    :host([color='error']) #button {
+      background-color: rgb(var(--arc-color-error));
     }
-    :host([type='contained'][color='error']) #button {
-      background-color: var(--arc-color-error);
+    :host([color='warning']) #button {
+      background-color: rgb(var(--arc-color-warning));
     }
-    :host([type='contained'][color='warning']) #button {
-      background-color: var(--arc-color-warning);
+    :host([color='info']) #button {
+      background-color: rgb(var(--arc-color-info));
     }
-    :host([type='contained'][color='info']) #button {
-      background-color: var(--arc-color-info);
-    }
-    :host([type='contained'][color='success']) #button {
-      background-color: var(--arc-color-success);
+    :host([color='success']) #button {
+      background-color: rgb(var(--arc-color-success));
     }
 
-    /* */
-
-    /* Tab */
-    :host([type='tab']) #button {
-      border: none;
-      border-radius: 0;
-      color: var(--arc-color-secondary);
-      min-width: var(--min-width);
-    }
-    /* Tab - Active */
-    :host([type='tab'][active]:not([disabled])) #button {
-      border-bottom: solid 2px currentColor;
+    /* Main - Hover */
+    :host(:not([type='tab']):not([disabled])) #button:hover {
+      background-image: linear-gradient(var(--arc-hover-dark) 0 0);
     }
 
-    /* Sizes */
-    :host([size='small']) #button {
-      height: var(--arc-input-height-small);
-    }
-
-    :host([size='small']) span {
-      padding: 0 var(--arc-spacing-small);
-    }
-
-    :host([size='medium']) #button {
-      height: var(--arc-input-height-medium);
-    }
-
-    :host([size='medium']) span {
-      padding: 0 var(--arc-spacing-medium);
-    }
-
-    :host([size='large']) #button {
-      height: var(--arc-input-height-large);
-    }
-
-    :host([size='large']) span {
-      padding: 0 var(--arc-spacing-large);
-    }
-
-    /* Disabled */
+    /* Main - Disabled */
     :host([disabled]) #button {
-      color: var(--arc-color-disabled);
-      background-color: var(--arc-background-disabled);
+      color: rgb(var(--arc-input-color)); !important;
+      background-image: linear-gradient(var(--arc-hover-dark) 0 0);
       opacity: 0.5;
       box-shadow: none;
       cursor: not-allowed;
     }
 
-    /* Hover */
-    :host([type='contained']:not([disabled])) #button:hover {
-      background-image: linear-gradient(rgba(0, 0, 0, 0.1) 0 0);
+    /* Tab */
+    :host([type='tab']) #button {
+      color: rgb(var(--arc-tab-color));
+      background: none;
+      border-radius: 0;
+      box-shadow: none;
+      min-width: var(--min-width);
     }
-    :host(:not([type='contained']):not([disabled])) #button:hover {
+    /* Tab - Active */
+    :host([type='tab']:not([disabled])[active]) #button {
+      border-bottom: solid 2px currentColor;
+    }
+    /* Tab - Hover */
+    :host([type='tab']:not([disabled])) #button:hover {
       background-color: currentColor;
-      -moz-box-shadow: inset 0 0 100px 100px rgba(var(--arc-red-060), 0.7);
-      -webkit-box-shadow: inset 0 0 100px 100px rgba(var(--arc-red-060), 0.7);
-      box-shadow: inset 0 0 100px 100px rgba(var(--arc-red-060), 0.7);
+      background-image: linear-gradient(var(--arc-hover-light) 0 0);
     }
+
+    /* Sizes */
+    :host([size='small']) #button {height: var(--arc-input-height-small);}
+
+    :host([size='small']) span {padding: 0 var(--arc-spacing-small);}
+
+    :host([size='medium']) #button {height: var(--arc-input-height-medium);}
+
+    :host([size='medium']) span {padding: 0 var(--arc-spacing-medium);}
+
+    :host([size='large']) #button {height: var(--arc-input-height-large);}
+
+    :host([size='large']) span {padding: 0 var(--arc-spacing-large);}
   `;
 
-  /** @type { 'contained' | 'outlined' | 'tab' } */
+  /** @type { 'contained' | 'tile' | 'outlined' | 'tab' } */
   @property({ type: String, reflect: true })
   type: string = 'contained';
 

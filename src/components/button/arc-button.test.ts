1e90da7d261eca264e45629089ffb31f9a0ebba9
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-16 17:45:06 +0200

    WIP

--- src/components/button/arc-button.test.ts
+++ src/components/button/arc-button.test.ts
@@ -7,15 +7,22 @@ import './arc-button.js';
 describe('ArcButton', () => {
   describe('rendering', () => {
     it('renders slots to fill the button', async () => {
-      const button: ArcButton = await fixture(html` <arc-button></arc-button> `)
+      const button: ArcButton = await fixture(html` <arc-button></arc-button> `);
       const buttonTarget = button.shadowRoot!.getElementById('button')!;
 
       expect(buttonTarget.querySelector('slot')).to.exist;
     })
+    it('renders a button with default properties', async () => {
+      const button: ArcButton = await fixture(html` <arc-button></arc-button> `);
+      expect(button).dom.to.equal(`<arc-button></arc-button>`);
+    })
 
     it('passes the a11y audit', async () => {
-      const button: ArcButton = await fixture(html` <arc-button>Test</arc-button> `)
+      const button: ArcButton = await fixture(html` <arc-button>Test</arc-button> `);
       await expect(button).shadowDom.to.be.accessible();
     });
   });
+  describe('states', () => {
+
+  })
 });

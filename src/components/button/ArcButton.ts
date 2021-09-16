cb6f6eb9c047bc998d8c82d02e074f3b88a3539d
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-16 17:20:15 +0200

    WIP: Fixed a bug for testing the web-components within the DOM properly

---
+++ src/components/button/ArcButton.ts
@@ -0,0 +1,172 @@
+import { css, html, LitElement } from 'lit';
+import { property, query } from 'lit/decorators.js';
+import { styleMap } from 'lit/directives/style-map.js';
+
+export class ArcButton extends LitElement {
+  static get tag() {
+    return 'arc-button';
+  }
+
+  static styles = css`
+    :host {
+      display: inline-block;
+      cursor: pointer;
+      width: auto;
+      --min-width: 0;
+    }
+
+    :host *, :host ::before, :host ::after {
+      box-sizing: inherit;
+    }
+
+    #button {
+      min-height: 100%;
+      min-width: var(--min-width);
+      width: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: var(--btn-color);
+      background-color: var(--btn-background);
+      border: none;
+      border-radius: var(--arc-border-radius-medium);
+      box-shadow: var(--arc-box-shadow);
+      font-family: var(--arc-font-button);
+      font-size: var(--arc-font-size-small);
+      font-weight: var(--arc-font-weight-semibold);
+      cursor: inherit;
+      text-decoration: none;
+      user-select: none;
+      line-height: normal;
+      outline: none;
+      padding: 0;
+      -webkit-appearance: none;
+      white-space: nowrap;
+    }
+
+    /* Disabled */
+    :host([disabled]) #button {
+      opacity: 0.5;
+      box-shadow: none;
+      cursor: default;
+    }
+
+    /* Hover */
+    :host(:not([type='tab']):not([type='outlined']):not([disabled])) #button:hover {
+      background-image: linear-gradient(var(--arc-hover-dark) 0 0);
+    }
+
+    /* Sizes */
+    :host([size='small']) #button {height: var(--arc-input-height-small);}
+
+    :host([size='small']) span {padding: 0 var(--arc-spacing-small);}
+
+    :host([size='medium']) #button {height: var(--arc-input-height-medium);}
+
+    :host([size='medium']) span {padding: 0 var(--arc-spacing-medium);}
+
+    :host([size='large']) #button {height: var(--arc-input-height-large);}
+
+    :host([size='large']) span {padding: 0 var(--arc-spacing-large);}
+
+    /* Radius */
+    :host([type='tile']) #button {border-radius: 0;}
+
+    :host([type='pill'][size='small']) #button {border-radius: var(--arc-input-height-small);}
+
+    :host([type='pill'][size='medium']) #button {border-radius: var(--arc-input-height-medium);}
+
+    :host([type='pill'][size='large']) #button {border-radius: var(--arc-input-height-large);}
+
+    /* Outlined */
+    :host([type='outlined']) #button {
+      border: var(--arc-border-width) var(--arc-border-style) currentColor;
+      background-color: transparent;
+      box-shadow: none;
+    }
+
+    /* Outlined - Hover */
+    :host([type='outlined']:not([disabled])) #button:hover {
+      background-color: currentColor;
+      background-image: linear-gradient(var(--arc-hover-light) 0 0);
+    }
+
+    /* Tab */
+    :host([type='tab']) #button {
+      background: none;
+      border-radius: 0;
+      box-shadow: none;
+    }
+    /* Tab - Active */
+    :host([type='tab']:not([disabled])[active]) #button {
+      border-bottom: var(--arc-border-width) var(--arc-border-style) currentColor;
+    }
+    /* Tab - Hover */
+    :host([type='tab']:not([disabled])) #button:hover {
+      background-color: currentColor;
+      background-image: linear-gradient(var(--arc-hover-light) 0 0);
+    }
+  `;
+
+  /** @type { 'contained' | 'tile' | 'outlined' | 'pill' | 'tab' } */
+  @property({ type: String, reflect: true })
+  type: string = 'contained';
+
+  /** @type { 'default' | 'primary' | 'secondary' | 'error' | 'warning' | 'info' | 'success' } */
+  @property({ type: String, reflect: true })
+  color: string = 'default';
+
+  /** @type { 'small' | 'medium' | 'large' } */
+  @property({type: String, reflect: true })
+  size: string = 'medium';
+
+  @property({type: Boolean, reflect: true})
+  active: boolean = false;
+
+  @property({type: Boolean, reflect: true})
+  disabled: boolean = false;
+
+  @property()
+  href = null;
+
+  @query('#button')
+  button!: HTMLSpanElement;
+
+  render() {
+    const compStyles = window.getComputedStyle(this);
+    const userDefinedColor = () => compStyles.getPropertyValue('--btn-color');
+    const userDefinedBackground = () => compStyles.getPropertyValue('--btn-background');
+
+    const getColor = () => {
+      switch (this.type) {
+        case 'outlined': {
+          return this.color === 'default'
+            ? 'rgb(var(--arc-font-color))'
+            : 'var(--btn-background)';
+        }
+        case 'tab': {
+          return this.color === 'default'
+            ? 'rgb(var(--arc-color-primary))'
+            : 'var(--btn-background)';
+        }
+        default: {
+          return this.color === 'primary' || this.color === 'secondary'
+            ? 'rgb(var(--arc-container-color))'
+            : 'rgb(var(--arc-input-color))'
+        }
+      }
+    }
+
+    const styles = {
+      '--btn-color': userDefinedColor().length > 0 ? null : getColor(),
+      '--btn-background': userDefinedBackground().length > 0 ? null : `rgb(var(--arc-color-${this.color}))`
+    };
+
+    return html`
+      ${this.href && !this.disabled
+      ? html`<a id='button' style=${styleMap(styles)} href='${this.href}' rel='noreferrer noopener' tabindex='-1'><span><slot></slot></span></a>`
+      : html`<button id='button' style=${styleMap(styles)} tabindex='-1'><span><slot></slot></span></button>`
+    }
+    `;
+  }
+}
--- src/components/button/arc-button.test.ts
+++ src/components/button/arc-button.test.ts
@@ -1,38 +1,23 @@
 import { html } from 'lit';
-import { expect, fixture } from '@open-wc/testing';
+import { expect, fixture, elementUpdated } from '@open-wc/testing';
 
-import { ArcButton } from './arc-button.js';
+import { ArcButton } from './ArcButton.js';
+import './arc-button.js';
 
 describe('ArcButton', () => {
   describe('rendering', () => {
-    let element: ArcButton;
-    beforeEach(async () => {
-      element = await fixture(html`<arc-button></arc-button>`)
-    })
+    it('renders slots to fill the button', async () => {
+      const button: ArcButton = await fixture(html` <arc-button></arc-button> `)
 
-    it('renders a slot to fill the button', () => {
-      const button = element.shadowRoot!.getElementById('button')!;
-      expect(button.querySelector('slot')).to.exist;
+      button.active = true;
+      await elementUpdated(button);
+
+      expect(button).dom.to.equal(`<arc-button active></arc-button>`)
     })
 
     it('passes the a11y audit', async () => {
-      await expect(element).shadowDom.to.be.accessible();
+      const button: ArcButton = await fixture(html` <arc-button>Hey</arc-button> `)
+      await expect(button).shadowDom.to.be.accessible();
     });
   });
-  describe('states', () => {
-    it('draws the button in an active state', async () => {
-      const element: ArcButton = await fixture(html`<arc-button active></arc-button>`);
-      expect(element.hasAttribute('active')).to.be.true;
-      expect(element.active).to.be.true;
-    });
-    it('draws the button in a disabled state', async () => {
-      const element: ArcButton = await fixture(html`<arc-button disabled></arc-button>`);
-      expect(element.hasAttribute('disabled')).to.be.true;
-      expect(element.disabled).to.be.true;
-    });
-    // TODO: Add this test once the arc-loader component is done
-    // it('draws the button in a loading state', async () => {
-    //   const element = await fixture(html`<arc-button></arc-button>`);
-    // });
-  })
-})
+});

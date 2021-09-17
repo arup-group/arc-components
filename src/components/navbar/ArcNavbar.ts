522bbbc38b97669acd3a5d8f1e0843bddbdc72b9
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-17 16:21:37 +0200

    WIP: Added some styling and built a classMap directive to set the position of the tabs based on the input of the position property

--- src/components/navbar/ArcNavbar.ts
+++ src/components/navbar/ArcNavbar.ts
@@ -1,11 +1,40 @@
 import { css, html, LitElement } from 'lit';
+import { property } from 'lit/decorators.js';
+import { classMap } from 'lit/directives/class-map.js';
 
 export class ArcNavbar extends LitElement {
   static tag = 'arc-navbar';
 
-  static styles = css``;
+  static styles = css`
+    :host, #main, #brand, #tabs {
+      display: flex;
+    }
+    #main {
+      min-width: 100%;
+    }
+    #tabs {
+      flex-basis: 100%;
+      flex-direction: row;
+    }
+    #tabs.right {
+      flex-direction: row-reverse;
+    }
+  `;
+
+  /** @type { 'left' | 'right' } */
+  @property({ type: String, reflect: true })
+  position: string = 'right';
 
   render() {
-    return html``
+    const classes = {
+      right: this.position === 'right',
+    };
+
+    return html`
+      <div id='main'>
+        <div id='brand'></div>
+        <div id='tabs' class=${classMap(classes)}><slot></slot></div>
+      </div>
+    `
   }
 }

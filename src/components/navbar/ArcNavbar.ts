9acf9daea2e7f1ffa54aeb3edd2c567ddab90007
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-17 15:14:21 +0200

    WIP: Building the ArcNavbar web-component

---
+++ src/components/navbar/ArcNavbar.ts
@@ -0,0 +1,11 @@
+import { css, html, LitElement } from 'lit';
+
+export class ArcNavbar extends LitElement {
+  static tag = 'arc-navbar';
+
+  static styles = css``;
+
+  render() {
+    return html``
+  }
+}

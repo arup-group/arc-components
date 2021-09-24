6f430fc6d04b9f6c2b67ca05899f7ff92658c52e
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-24 17:00:09 +0200

    WIP

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,3 @@
-import '!style-loader!styles-loader!sass-loader!../src/components/styles/index/main.scss';
+import '!style-loader!styles-loader!sass-loader!../src/styles/index.css';
+import '!style-loader!styles-loader!sass-loader!../src/themes/light.css';
+import '!style-loader!styles-loader!sass-loader!../src/themes/dark.css';

c10aaec5956d1102948e66c0fdd7a41324aa3299
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-28 14:55:38 +0200

    Moved the margin from the tabs to the Arup logo as the Arup logo is optional WIP: writing the test for the arc-navbar

--- src/components/navbar/arc-navbar.test.ts
+++ src/components/navbar/arc-navbar.test.ts
@@ -5,5 +5,20 @@ import { ArcNavbar } from './ArcNavbar.js';
 import './arc-navbar.js';
 
 describe('ArcNavbar', () => {
-  describe('rendering', () => {});
+  describe('rendering', () => {
+    let element: ArcNavbar;
+    beforeEach(async () => {
+      element = await fixture(html`<arc-navbar></arc-navbar>`);
+    });
+
+    it('renders slots to fill the tabs', () => {
+      const tabContainer = element.shadowRoot!.getElementById('tabs')!;
+
+      expect(tabContainer.querySelector('slot')).to.exist;
+    })
+
+    it('passes the a11y audit', async () => {
+      await expect(element).shadowDom.to.be.accessible();
+    });
+  });
 })

778d5eaa2fd0584f6e57141c395b16f55fe32db6
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-28 10:47:54 +0200

    WIP: Trying to solve the styling of Storybook

--- .storybook/preview.js
+++
@@ -1,3 +0,0 @@
-import '!style-loader!styles-loader!sass-loader!../src/styles/index.css';
-import '!style-loader!styles-loader!sass-loader!../src/themes/light.css';
-import '!style-loader!styles-loader!sass-loader!../src/themes/dark.css';

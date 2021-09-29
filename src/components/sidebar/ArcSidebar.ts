a1023e85e910d25f0246a9375aabeacc9c2b4fad
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-29 16:51:31 +0200

    WIP: Added some functionality to automatically add a gap between slotted sidebar items

--- src/components/sidebar/ArcSidebar.ts
+++ src/components/sidebar/ArcSidebar.ts
@@ -1,4 +1,5 @@
 import { css, html, LitElement } from 'lit';
+import { query } from 'lit/decorators.js';
 import { componentStyles } from '../styles/component.styles.js';
 
 export class ArcSidebar extends LitElement {
@@ -9,12 +10,18 @@ export class ArcSidebar extends LitElement {
     css`
       :host {
         --width: 23rem;
+        --gap-distance: var(--arc-spacing-normal);
       }
 
       /* Layout */
       #main {
         height: 100%;
         width: var(--width);
+        display: grid;
+      }
+
+      #main.gap {
+        gap: var(--gap-distance);
       }
 
       ::slotted(*) {
@@ -23,10 +30,21 @@ export class ArcSidebar extends LitElement {
     `
   ]
 
+  @query('#main')
+  main!: HTMLElement;
+
+  handleSlots = (e: any) => {
+    const childNodes = e.target.assignedElements({flatten: true});
+
+    if (childNodes.length > 1) {
+      this.main.classList.add('gap')
+    }
+  }
+
   render() {
     return html`
       <main id='main'>
-        <slot></slot>
+        <slot @slotchange=${this.handleSlots}></slot>
       </main>
     `
   }

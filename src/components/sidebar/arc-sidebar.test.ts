e00e8b575fad6a73ab9d3c04531b3584175cb7c2
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-09-30 17:21:07 +0200

    Finished the first test for the sidebar component

--- src/components/sidebar/arc-sidebar.test.ts
+++ src/components/sidebar/arc-sidebar.test.ts
@@ -7,14 +7,25 @@ import './arc-sidebar.js';
 describe('ArcSidebar', () => {
   describe('rendering', () => {
     let element: ArcSidebar;
+    let main: HTMLElement;
+
     beforeEach(async () => {
       element = await fixture(html`<arc-sidebar></arc-sidebar>`);
+      main = element.shadowRoot!.getElementById('main')!;
     });
 
     it('renders a slot to fill the sidebar', () => {
-      const main = element.shadowRoot!.getElementById('main')!;
-
       expect(main.querySelector('slot')).to.exist;
+    })
+
+    it('adds a gap between multiple slots', async () => {
+      element.innerHTML = `
+        <div>Slotted content 1</div>
+        <div>Slotted content 2</div>
+      `;
+      await elementUpdated(element);
+
+      expect(main.classList.contains('gap')).to.be.true;
     })
 
     it('passes the a11y audit', async () => {
@@ -24,14 +35,19 @@ describe('ArcSidebar', () => {
   describe('css variables', () => {
     it('uses the default css variables', async () => {
       const element: ArcSidebar = await fixture(html`<arc-sidebar></arc-sidebar>`);
+      const elementStyles = window.getComputedStyle(element);
 
+      expect(elementStyles.getPropertyValue('--gap-distance')).to.equal('');
+      expect(elementStyles.getPropertyValue('width')).to.equal('auto');
     });
-    it('overwrites the width', async () => {
-      const element: ArcSidebar = await fixture(html`<arc-sidebar></arc-sidebar>`);
+    it('overwrites the css variables', async () => {
+      const element: ArcSidebar = await fixture(html`
+        <arc-sidebar style='--gap-distance:2rem; width:368px'></arc-sidebar>
+      `);
+      const elementStyles = window.getComputedStyle(element);
 
+      expect(elementStyles.getPropertyValue('--gap-distance')).to.equal('2rem');
+      expect(elementStyles.getPropertyValue('width')).to.equal(`368px`);
     });
-    it('overwrites the gap', async () => {
-
-    })
   })
 })

d56377fcb62a5edcb7252a0afc533ab5014d9af6
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-10-20 10:52:32 +0200

    Rewriting the imports of the stories.mdx files

--- .storybook/server.mjs
+++ .storybook/server.mjs
@@ -1,8 +1,18 @@
 import { storybookPlugin } from '@web/dev-server-storybook';
 import baseConfig from '../web-dev-server.config.mjs';
+import copy from 'rollup-plugin-copy';
 
 export default /** @type {import('@web/dev-server').DevServerConfig} */ ({
   ...baseConfig,
   open: '/',
-  plugins: [storybookPlugin({ type: 'web-components' }), ...baseConfig.plugins],
+  plugins: [
+    storybookPlugin({ type: 'web-components' }),
+    /** Copy the styles */
+    copy({
+      targets: [{ src: '../src/css/**/*', dest: './dist' }],
+      // set flatten to false to preserve folder structure
+      flatten: false,
+    }),
+    ...baseConfig.plugins
+  ],
 });

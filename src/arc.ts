40981a52bd2943eccaf30c6fe8a7859ff127e1f7
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-10-22 13:08:16 +0200

    Added two files

---
+++ src/arc.ts
@@ -0,0 +1,6 @@
+import { default as ArcButton } from './components/button/arc-button.js';
+import { default as ArcContainer } from './components/container/arc-container.js';
+import { default as ArcDivider } from './components/divider/arc-divider.js';
+import { default as ArcIcon } from './components/icon/arc-icon.js';
+import { default as ArcNavbar } from './components/navbar/arc-navbar.js';
+import { default as ArcSidebar } from './components/sidebar/arc-sidebar.js';

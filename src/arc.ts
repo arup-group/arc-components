1a9709bbdd03b032d2ffeab2265519ecf90df4b1
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-11-12 13:02:38 +0100

    Removed a missing import Sorted the icon types by alphabetical order

--- src/arc.ts
+++ src/arc.ts
@@ -1,7 +1,6 @@
 /* Components */
 export { default as ArcButton } from './components/button/ArcButton.js';
 export { default as ArcContainer } from './components/container/ArcContainer.js';
-export { default as ArcDivider } from './components/divider/ArcDivider.js';
 export { default as ArcIcon } from './components/icon/ArcIcon.js';
 export { default as ArcIconButton } from './components/icon-button/ArcIconButton.js';
 export { default as ArcNavbar } from './components/navbar/ArcNavbar.js';

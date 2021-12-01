13fe501fab9c79100e876ef4528540924f00a8c7
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-12-01 16:43:22 +0100

    Fixed the stories that got removed during a merging process

--- stories/arc-icon.stories.d.ts
+++
@@ -1,9 +0,0 @@
-import '../src/components/icon/arc-icon.js';
-
-export declare const Default: any;
-export declare const VariableSize: any;
-export declare const RedColor: any;
-export declare const GreenColor: any;
-export declare const BlueColor: any;
-export declare const PurpleColor: any;
-export declare const CustomSize: any;
--- stories/arc-menu-item.stories.d.ts
+++
@@ -1,6 +0,0 @@
-import '../src/components/menu-item/arc-menu-item.js';
-import '../src/components/icon/arc-icon.js';
-
-export declare const Default: any;
-export declare const PrefixSuffix: any;
-export declare const Disabled: any;

3fe62c3ad6c5a9a2949ba42f8ad948dce1fe0a94
Jasper.Wieringa <jasperwieringa@gmail.com>
2021-12-08 11:19:02 +0100

    Added some better comments to the watch utility. Added the tests for the watch utility

--- src/internal/watch.ts
+++ src/internal/watch.ts
@@ -1,12 +1,29 @@
 /* @watch decorator
-Runs when an observed property changes, e.g. @property or @state, but before the component updates.
-To wait for an update to complete after a change occurs, use `await this.updateComplete` in the handler. To start
-watching after the initial update/render, use `{ waitUntilFirstUpdate: true }` or `this.hasUpdated` in the handler.
-Usage:
- @watch('propName')
- handlePropChange(oldValue, newValue) {
-   ...
- }
+Runs when @property or @state changes but before the component updates in the DOM.
+To wait for an update to complete after a change occurs, use `await this.updateComplete` in the handler.
+Example:
+connectedCallback() {
+  super.connectedCallback();
+  this.updateComplete.then(() => {
+    ...
+  })
+}
+
+To start watching for a property to change after the first update/render, use `{ waitUntilFirstUpdate: true }`.
+You can also use `this.hasUpdated` in the handler.
+Example waitUntilFirstUpdate:
+@watch('propName', { waitUntilFirstUpdate: true })
+handlePropChange(oldVal, newVal) {
+  ...
+}
+
+Example this.hasUpdated:
+@watch('propName')
+handlePropChange(oldVal, newVal) {
+  if(this.hasUpdated) {
+    ...
+  }
+}
 */
 interface WatchOptions {
   waitUntilFirstUpdate?: boolean;

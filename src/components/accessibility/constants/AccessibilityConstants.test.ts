c403d234a10f063020873cb75d82a83ce3434b70
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-10 13:30:02 +0100

    Removed the unnecessary AccessibilityConstants tests

--- src/components/accessibility/constants/AccessibilityConstants.test.ts
+++ src/components/accessibility/constants/AccessibilityConstants.test.ts
@@ -1,39 +1,16 @@
 import { expect } from '@open-wc/testing';
 
-import { ACCESSIBILITY_OPTIONS, AccessibilityKey, USER_PREFERENCES } from './AccessibilityConstants.js';
+import { ACCESSIBILITY_OPTIONS, AccessibilityKey } from './AccessibilityConstants.js';
 
-describe('AccessibilityConstants', () => {
-  describe('AccessibilityOptions', () => {
-    it('should contain the basic ARC options', () => {
-      expect(ACCESSIBILITY_OPTIONS).to.exist;
-      const keys = Object.keys(ACCESSIBILITY_OPTIONS);
+describe('AccessibilityOptions', () => {
+  it('should contain the basic ARC options', () => {
+    expect(ACCESSIBILITY_OPTIONS).to.exist;
+    const keys = Object.keys(ACCESSIBILITY_OPTIONS);
 
-      expect(keys.length).to.equal(3);
-      keys.forEach((key: AccessibilityKey) => {
-        expect(Object.keys(ACCESSIBILITY_OPTIONS[key])).to.contain('icon');
-        expect(Object.keys(ACCESSIBILITY_OPTIONS[key])).to.contain('values');
-      });
-    });
-  });
-
-  describe('UserPreferences', () => {
-    it('should contain the basic ARC preferences', () => {
-      expect(USER_PREFERENCES).to.exist;
-      const keys = Object.keys(USER_PREFERENCES);
-
-      expect(keys.length).to.equal(3);
-      expect(keys).to.contain('colourMode');
-      expect(keys).to.contain('textSize');
-      expect(keys).to.contain('textDisplay');
-    });
-
-    it('should contain a set of text display options', () => {
-      const keys = Object.keys(USER_PREFERENCES.textDisplay);
-
-      expect(keys.length).to.equal(3);
-      expect(keys).to.contain('highLegibilityFonts');
-      expect(keys).to.contain('highlightLinks');
-      expect(keys).to.contain('plainText');
+    expect(keys.length).to.equal(3);
+    keys.forEach((key: AccessibilityKey) => {
+      expect(Object.keys(ACCESSIBILITY_OPTIONS[key])).to.contain('icon');
+      expect(Object.keys(ACCESSIBILITY_OPTIONS[key])).to.contain('values');
     });
   });
 });

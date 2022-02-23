957359a6328b6524b307f3e4d6fbcc17e6c3c0c6
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-02-23 13:20:03 +0100

    WIP: Adding the plop configuration to create web-components with a simple script.

---
+++ scripts/plop/plopfile.mjs
@@ -0,0 +1,51 @@
+export default function (plop) {
+  plop.setHelper('tagWithoutPrefix', tag => tag.replace(/^arc-/, ''));
+
+  plop.setHelper('tagToTitle', tag => {
+    const withoutPrefix = plop.getHelper('tagWithoutPrefix');
+    const titleCase = plop.getHelper('titleCase');
+    return titleCase(withoutPrefix(tag));
+  });
+
+  plop.setGenerator('component', {
+    description: 'Generate a new ARC web-component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'tag',
+        message: 'Tag name? (e.g. arc-button)',
+        validate: value => {
+          // Start with arc- and include only a-z + dashes
+          if (!/^arc-[a-z-+]+/.test(value)) {
+            return false;
+          }
+
+          // No double dashes or ending dash
+          return !(value.includes('--') || value.endsWith('-'));
+        }
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../../src/components/{{ tagWithoutPrefix tag }}/{{ properCase tag }}.ts',
+        templateFile: './templates/component.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../src/components/{{ tagWithoutPrefix tag }}/{{ tag }}.ts',
+        templateFile: './templates/registry.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../src/components/{{ tagWithoutPrefix tag }}/{{ tag }}.test.ts',
+        templateFile: './templates/tests.hbs'
+      },
+      {
+        type: 'add',
+        path: '../../src/components/{{ tagWithoutPrefix tag }}/{{ tag }}.stories.ts',
+        templateFile: './templates/story.hbs'
+      }
+    ]
+  });
+};

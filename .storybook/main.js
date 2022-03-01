d33ef2a14aac2f3ebcd7226d8efdd85c9cb17299
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-03-01 13:34:29 +0100

    WIP: Setting up a new approach for the Storybook documentation.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,11 @@
 module.exports = {
-  stories: ['../stories/*.stories.mdx'],
-};
+  "stories": [
+    "../out-tsc/**/*.stories.mdx",
+    "../out-tsc/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  "framework": "@storybook/web-components"
+}

60333f4285bc374f051f0a4b9c40e43b37388a55
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-03-10 14:58:02 +0100

    Added the themePalette.ts that can be used for integration with the MUI themeprovider

---
+++ src/styles/themePalette.ts
@@ -0,0 +1,21 @@
+export const themePalette = {
+  primary: {
+    main: 'rgb(var(--arc-color-primary))'
+  },
+  error: {
+    main: 'rgb(var(--arc-color-error))'
+  },
+  warning: {
+    main: 'rgb(var(--arc-color-warning))'
+  },
+  info: {
+    main: 'rgb(var(--arc-color-info))'
+  },
+  success: {
+    main: 'rgb(var(--arc-color-success))'
+  },
+  background: {
+    default: 'rgb(var(--arc-background-color))',
+    paper: 'rgb(var(--arc-container-color))'
+  }
+}

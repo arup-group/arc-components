933dc21902a086c0d50d0979ec623bcc45dc854c
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-03-24 09:09:35 +0100

    Updated the card story with some more meaningful <style> tags

--- src/components/card/arc-card.stories.ts
+++ src/components/card/arc-card.stories.ts
@@ -15,9 +15,9 @@ export default {
 } as Meta;
 
 const Template: Story<ArcCard> = ({ collapsed }) => html`
-  <arc-card style="width: 20rem" ?collapsed=${collapsed}>
+  <arc-card class="card" ?collapsed=${collapsed}>
     <div slot="header">
-      <div style="display: grid">
+      <div>
         <strong>Header</strong>
         <small>Subheading</small>
       </div>
@@ -28,27 +28,37 @@ const Template: Story<ArcCard> = ({ collapsed }) => html`
       src="https://images.unsplash.com/photo-1646882172899-8436f0219b71?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
       alt="A picture of two birds."
     />
-    <div style="display: grid">
-      <strong>Header</strong>
-      <small>Subheading</small>
-    </div>
-    <p>These birds are as cute as they are playful!</p>
+    These birds are as cute as they are playful!
     <div slot="footer">
       <arc-button>Action</arc-button>
     </div>
   </arc-card>
+  <style>
+    .card {
+      width: 20rem;
+    }
+
+    .card [slot='header'] div {
+      display: grid;
+    }
+  </style>
 `;
 
 const BasicTemplate: Story<ArcCard> = ({ collapsed }) => html`
-  <arc-card style="width: 20rem" ?collapsed=${collapsed}>
+  <arc-card class="card" ?collapsed=${collapsed}>
     This is just a basic card. No image, no header, no footer, just content.
   </arc-card>
+  <style>
+    .card {
+      width: 20rem;
+    }
+  </style>
 `;
 
 const HeaderTemplate: Story<ArcCard> = ({ collapsed }) => html`
-  <arc-card style="width: 20rem" ?collapsed=${collapsed}>
+  <arc-card class="card" ?collapsed=${collapsed}>
     <div slot="header">
-      <div style="display: grid">
+      <div>
         <strong>Header</strong>
         <small>Subheading</small>
       </div>
@@ -56,19 +66,33 @@ const HeaderTemplate: Story<ArcCard> = ({ collapsed }) => html`
     </div>
     This card has a header. You can put all sorts of things in it!
   </arc-card>
+  <style>
+    .card {
+      width: 20rem;
+    }
+
+    .card [slot='header'] div {
+      display: grid;
+    }
+  </style>
 `;
 
 const FooterTemplate: Story<ArcCard> = ({ collapsed }) => html`
-  <arc-card style="width: 20rem" ?collapsed=${collapsed}>
+  <arc-card class="card" ?collapsed=${collapsed}>
     This card has a footer. You can put all sorts of things in it!
     <div slot="footer">
       <arc-button>Action</arc-button>
     </div>
   </arc-card>
+  <style>
+    .card {
+      width: 20rem;
+    }
+  </style>
 `;
 
 const ImageTemplate: Story<ArcCard> = ({ collapsed }) => html`
-  <arc-card style="width: 20rem" ?collapsed=${collapsed}>
+  <arc-card class="card" ?collapsed=${collapsed}>
     <img
       slot="image"
       src="https://images.unsplash.com/photo-1644990978896-c86bd2e5fd2e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1166&q=80"
@@ -76,14 +100,19 @@ const ImageTemplate: Story<ArcCard> = ({ collapsed }) => html`
     />
     This eagle is a symbol for strength and freedom.
   </arc-card>
+  <style>
+    .card {
+      width: 20rem;
+    }
+  </style>
 `;
 
 /* EXAMPLES */
 export const Default = Template.bind({});
-export const Basic = BasicTemplate.bind({});
-export const Header = HeaderTemplate.bind({});
-export const Footer = FooterTemplate.bind({});
-export const Image = ImageTemplate.bind({});
+export const BasicCard = BasicTemplate.bind({});
+export const CardWithHeader = HeaderTemplate.bind({});
+export const CardWithFooter = FooterTemplate.bind({});
+export const CardWithImage = ImageTemplate.bind({});
 
 /* STATES */
 export const Collapsed = Template.bind({});

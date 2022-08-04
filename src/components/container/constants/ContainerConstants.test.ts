06405cf1ba4678667d25db912061b348345a34db
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-08-04 10:44:18 +0200

    Added the arc-markdown to the `ignore` list when handling the keyDown event within the ArcContainer component

--- src/components/container/constants/ContainerConstants.test.ts
+++ src/components/container/constants/ContainerConstants.test.ts
@@ -1,6 +1,6 @@
 import { expect } from '@open-wc/testing';
 
-import { CONTAINER_THEMES } from './ContainerConstants.js';
+import { CONTAINER_THEMES, IGNORE_KEYPRESS } from './ContainerConstants.js';
 
 describe('ContainerThemes', () => {
   it('should contain the basic Arc themes', () => {
@@ -11,5 +11,18 @@ describe('ContainerThemes', () => {
     expect(keys).to.contain('auto');
     expect(keys).to.contain('dark');
     expect(keys).to.contain('light');
+  });
+});
+
+describe('IgnoreKeypress', () => {
+  it('should contain form related elements to exclude from document keypress events', () => {
+    expect(IGNORE_KEYPRESS).to.exist;
+    const keys = Object.keys(IGNORE_KEYPRESS);
+
+    expect(keys.length).to.be.greaterThan(0);
+    expect(keys).to.contain('INPUT');
+    expect(keys).to.contain('TEXTAREA');
+    expect(keys).to.contain('ARC-MENU-ITEM');
+    expect(keys).to.contain('ARC-MARKDOWN');
   });
 });
--- src/components/container/constants/ContainerConstants.ts
+++ src/components/container/constants/ContainerConstants.ts
@@ -7,4 +7,4 @@ export const CONTAINER_THEMES: { [key in ContainerTheme]: ContainerTheme } = {
 };
 
 /* An array of elements that are excluded from the arc-container handleKeyDown event. */
-export const IGNORE_KEYPRESS: string[] = ['INPUT', 'TEXTAREA', 'ARC-MENU-ITEM'];
+export const IGNORE_KEYPRESS: string[] = ['INPUT', 'TEXTAREA', 'ARC-MENU-ITEM', 'ARC-MARKDOWN'];

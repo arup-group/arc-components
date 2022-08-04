dbda07ae5d6ddb8cf90b545fb9e0498084f8480b
Jasper.Wieringa <jasper.wieringa@arup.com>
2022-08-04 15:50:23 +0200

    Removed the keydown listener on the arc-container component as it causes unwanted behaviour

--- src/components/container/constants/ContainerConstants.ts
+++ src/components/container/constants/ContainerConstants.ts
@@ -5,6 +5,3 @@ export const CONTAINER_THEMES: { [key in ContainerTheme]: ContainerTheme } = {
   dark: 'dark',
   light: 'light',
 };
-
-/* An array of elements that are excluded from the arc-container handleKeyDown event. */
-export const IGNORE_KEYPRESS: string[] = ['INPUT', 'TEXTAREA', 'ARC-MENU-ITEM'];

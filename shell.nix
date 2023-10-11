71aa084f2c89f06186699c74a72ddb6dc80fd803
Dominic Egginton <dom.egginton@arup.com>
2023-10-11 11:12:16 +0100

    feat(packages/infrastucture): create infrastructure package
    Creates a new package in the `packages` directory called `infrastructure` that contains the
    infrastucture as code for ARC managed by Terraform.

--- shell.nix
+++ shell.nix
@@ -1,7 +1,11 @@
 with import <nixpkgs> {};
 
 pkgs.mkShell {
-  nativeBuildInputs = with pkgs.buildPackages; [ nodejs_18 ];
+  nativeBuildInputs = with pkgs.buildPackages; [
+    nodejs_18
+    terraform
+    azure-cli
+  ];
 
   shellHook = ''
     export PATH=$PATH:$(npm bin)

e975f861d85a2c37b186c34d694d16e5aa711305
Dom Egginton <dominic.egginton@gmail.com>
2023-10-21 15:40:11 +0100

    feat(infrastructure): import azure resrouce group (#194)
    * feat(infrastructure): import azure resrouce group
    
    * chore(infrastructure): format terraform files

--- packages/infrastructure/src/main.tf
+++ packages/infrastructure/src/main.tf
@@ -11,3 +11,23 @@ provider "azurerm" {
   skip_provider_registration = true
   features {}
 }
+
+resource "azurerm_resource_group" "arup-arc-infra-prod-rg" {
+  name     = "arup-arc-infra-prod-rg"
+  location = "UK South"
+  tags = {
+    Application        = "ARC project needs cloud infra resources"
+    CostCenter         = "01-89915"
+    CreationDate       = "2023-10-20T17:34:44Z"
+    Creator            = "arup-terraform-production-open"
+    Criticality        = "Not Defined"
+    Environment        = "prod"
+    FinanceAdmin       = "liz.lane@arup.com"
+    JobNumber          = "071101-42"
+    OnPremConnectivity = "No"
+    Owner              = "daragh.anderson@arup.com"
+    repo               = "arup-arc-infra-rg"
+    RequestNumber      = "RITM0730296"
+    Source             = "Terraform"
+  }
+}

65ff160ff24377832b4d017f17373981200c80da
Dominic Egginton <dom.egginton@arup.com>
2023-12-31 11:27:57 +0000

    chore(workspace): set nixpkgs to nixos-23.11 branch and update deps

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.systems.url = "github:nix-systems/default";
 
   outputs = {

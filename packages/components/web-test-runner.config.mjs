9bc3285a9da4d1f75842bbb7e61c3d6a7bd9f338
Thomas Marr <thomas.marr@arup.com>
2024-02-03 17:58:09 +0000

    fix(test): ensure stylesheets load in test environment

--- packages/components/web-test-runner.config.mjs
+++ packages/components/web-test-runner.config.mjs
@@ -12,11 +12,21 @@ const CONFIG = {
   filterBrowserLogs(log) {
     for (const arg of log.args) {
       return !['Lit is in dev mode'].some((filteredLog) =>
-        arg.includes(filteredLog),
+        arg?.includes(filteredLog),
       );
     }
     return true;
   },
-};
+  testRunnerHtml: testFramework =>
+    `<html>
+      <head>
+        <link rel="stylesheet" href="./packages/components/themes/index.css">
+        <link rel="stylesheet" href="./packages/components/themes/light.css">
+        <link rel="stylesheet" href="./packages/components/themes/dark.css">
+      </head>
+      <body>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`};
 
 export default CONFIG;

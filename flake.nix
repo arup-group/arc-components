89a5a3c8965e9ced5dcbefa2c66c698f32d5fbae
Dominic Egginton <dom.egginton@arup.com>
2024-02-22 17:52:57 +0000

    docs

--- flake.nix
+++ flake.nix
@@ -23,6 +23,7 @@
         ];
       });
 
+    # The node package to use for this workspace.
     node = forAllSystems (
       system:
         nixpkgsFor.${system}.nodejs_20
@@ -34,7 +35,10 @@
     );
     overlays = {
       default = final: prev: let
+        # Ignore scripts when installing npm packages to avoid encountering issues
+        # with the postinstall scripts of some packages.
         npmCommands = ["npm install --loglevel=verbose --no-fund --ignore-scripts"];
+
         nodejs = node.${final.system};
       in {
         components = final.noxide.buildPackage ./. {
@@ -76,16 +80,38 @@
       inherit (nixpkgsFor.${system}) components react storybook;
     });
 
-    devShells = forAllSystems (system: {
-      default = nixpkgsFor.${system}.mkShell {
-        packages = with nixpkgsFor.${system}; [
-          node.${system}
-          google-chrome
-        ];
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+      inherit (pkgs) mkShell;
+      inherit (pkgs.stdenv) isLinux;
+      inherit (pkgs.lib) optional;
+    in {
+      # The default development shell for this
+      # workspace. Google Chrome is required for
+      # running tests on the components package.
+      # The nixpkgs.google-chrome package is only
+      # available on Linux systems so we use the
+      # optional function to conditionally include
+      # it. Please ensure that your system has
+      # Google Chrome installed if you are using
+      # a different system than Linux.
+      default = mkShell {
+        packages = with pkgs;
+          [node.${system}]
+          ++ optional isLinux [google-chrome];
+
+        shellHook = ''
+          ${pkgs.lib.optionalString (!isLinux) ''
+            echo "Google Chrome is required for running tests on the components package."
+            echo "Please ensure that your system has Google Chrome installed."
+          ''}
+        '';
       };
 
-      infrastructure = nixpkgsFor.${system}.mkShell {
-        packages = with nixpkgsFor (system: nixpkgs.${system}); [
+      # A shell for interacting with infrastructure
+      # defined within this workspace.
+      infrastructure = mkShell {
+        packages = with pkgs; [
           node.${system}
           terraform
           terraform-ls

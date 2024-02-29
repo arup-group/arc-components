14846a03fd01b3443939e659197e3a90c94faebc
Dominic Egginton <dom.egginton@arup.com>
2024-02-29 12:11:59 +0000

    temp

---
+++ apps/docs/docusaurus.config.js
@@ -0,0 +1,89 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'ARC',
+  tagline: 'Arup Reuable Components',
+  url: 'https://arc.arup.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'arup-group',
+  projectName: 'arc',
+  presets: [
+    [
+      'classic',
+      ({
+        docs: { sidebarPath: require.resolve('./sidebars.js') },
+        blog: { showReadingTime: true },
+        theme: { customCss: require.resolve('./src/css/custom.css') },
+      }),
+    ],
+  ],
+  themeConfig:
+    ({
+      navbar: {
+        title: 'ARC',
+        logo: {
+          alt: 'ARC Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'introduction',
+            position: 'left',
+            label: 'Documentation',
+          },
+          {to: '/blog', label: 'Blog', position: 'left'},
+          {
+            href: 'https://github.com/arup-grou/arc-components',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/arup-group/arc-components',
+              },
+            ],
+          },
+        ],
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;

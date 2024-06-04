6975fc144db65c958ab826be9cf34c7f3174130e
Dominic Egginton <dom.egginton@arup.com>
2024-06-04 09:58:29 +0100

    fix(react/checbox): update import statement

--- packages/react/src/components/arc-checkbox.ts
+++ packages/react/src/components/arc-checkbox.ts
@@ -1,13 +1,12 @@
 import { createComponent, EventName } from '@lit-labs/react';
 import React from 'react';
 import { ARC_EVENTS } from '@arc-web/components/src/internal/constants/eventConstants.js';
-import ArcCheckBoxWC from '@arc-web/components/src/components/checkbox/ArcCheckBox';
-
+import ArcCheckboxWC from '@arc-web/components/src/components/checkbox/ArcCheckbox';
 import '@arc-web/components/src/components/checkbox/arc-checkbox.js';
 
-export const ArcCheckBox = createComponent({
+export const ArcCheckbox = createComponent({
   tagName: 'arc-checkbox',
-  elementClass: ArcCheckBoxWC,
+  elementClass: ArcCheckboxWC,
   react: React,
   events: {
     onArcChange: ARC_EVENTS.change as EventName<CustomEvent<{}>>,

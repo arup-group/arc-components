1644989adedbf97c9bfccab829166350f79d4f1b
Dominic Egginton <dom.egginton@arup.com>
2024-06-11 17:09:45 +0100

    chore(documentation): update docs and fix docs build

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -42,7 +42,6 @@ const config: Config = {
           position: 'left',
           label: 'Documentation',
         },
-        { to: '/blog', label: 'Blog', position: 'left' },
         {
           href: 'https://github.com/arup-group/arc-components',
           label: 'GitHub',
--- documentation/src/pages/index.tsx
+++ documentation/src/pages/index.tsx
@@ -1,5 +1,15 @@
 import Layout from "@theme/Layout";
 
 export default function Home(): JSX.Element {
-  return <Layout title="DTA Teambook" description=""></Layout>;
+  return (
+    <Layout
+      title="Arup Reuseable Components"
+      description=""
+    >
+      <div style={{ padding: 16 }}>
+        <h1>ARC</h1>
+        <p>Arup Reuseable Components</p>
+      </div>
+    </Layout>
+  );
 }

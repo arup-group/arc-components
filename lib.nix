eca0a029b13256d6e951685682720bf4aba79a3c
Dominic Egginton <dom.egginton@arup.com>
2024-07-08 15:38:23 +0100

    deps(workspace): bump @storybook/*** dev dep to address cve in ip

--- lib.nix
+++ lib.nix
@@ -33,7 +33,7 @@ in
         # this workspace is a monorepo and all dependencies
         # are resolved via the workspace root package.json
         src = cleanSource ./.;
-        npmDepsHash = "sha256-JxdVXbrrxwtcMK9e/Oa+TD0bgPEpqaeQxJSQkRh22DY=";
+        npmDepsHash = "sha256-1fyI0OCCi9tnB0iGTo2xQjHrJigD/+F3O6InsCtmLyE=";
 
         # dont run the build scripts when rebuilding
         # npm dependencies as node-keytar will fail
--- playgrounds/vanilla/src/main.ts
+++ playgrounds/vanilla/src/main.ts
@@ -1,4 +1,5 @@
 import '@arc-web/components/themes/index.css';
+import '@arc-web/components/src/components/container/arc-container';
 
 const app = document.querySelector('app-root')!;
 const container = document.createElement('arc-container');

68e7d617229d03ac3516e18d9c79bcd72cff575f
Dominic Egginton <dom.egginton@arup.com>
2024-07-22 09:28:08 +0100

    chore(workspace): run flake checks on all platforms

--- documentation/default.nix
+++ documentation/default.nix
@@ -1,12 +1,6 @@
-{ pkgs }:
+{ lib }:
 
-let
-  lib = import ../lib.nix { inherit pkgs; };
-in
-
-with lib;
-
-buildArcPackage {
+lib.buildArcPackage {
   name = "documentation";
 
   # run the documentation build script

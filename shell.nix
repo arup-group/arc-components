2e232385cffb6a24f6ed2a717b033efbdc9ced70
Dominic Egginton <dom.egginton@arup.com>
2024-08-19 13:58:20 +0100

    chore(workspace): update nix shell

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
-{ pkgs }:
+{ pkgs, mkShell, prefetch-npm-deps }:
 
-pkgs.mkShell {
-  buildInputs = with pkgs; [ nodejs azure-cli ];
-
+mkShell {
+  inputsFrom = with pkgs; [ components ];
+  packages = with pkgs; [ opentofu azure-cli ];
   shellHook = ''
     export PATH=$PATH:$(npm bin)
   '';

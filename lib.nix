a69610520564d3a5d6dde3c97011df4e7ff1eaea
Dominic Egginton <dom.egginton@arup.com>
2024-08-27 17:54:48 +0100

    chore(workspace): update npm deps hash

--- lib.nix
+++ lib.nix
@@ -33,7 +33,7 @@ in
         # this workspace is a monorepo and all dependencies
         # are resolved via the workspace root package.json
         src = cleanSource ./.;
-        npmDepsHash = "sha256-PHnuZ8Foj9zAShK0G6z+xUrABPW0ukpCchTuqOEDfOc=";
+        npmDepsHash = "sha256-nVW4HWzLz58lnWjWvwvyfE24cKIj1baoDpgBmPhuvMI=";
 
         # dont run the build scripts when rebuilding
         # npm dependencies as node-keytar will fail

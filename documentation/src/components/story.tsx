25fba3bd8a77c6c0b4ce2efb340f70fc98631f95
Dominic Egginton <dom.egginton@arup.com>
2024-08-28 16:05:58 +0100

    chore: update pr

--- documentation/src/components/story.tsx
+++ documentation/src/components/story.tsx
@@ -2,17 +2,10 @@ import React from 'react'
 
 const FRAME_STYLE = { border: '1px solid #aaa' };
 
-/* TODO:
-  * 1. props for component & story
-  * 2. iframe src should be dynamic
-  * 3. make storybook iframe not render sidebar controls
-  */
-export default function Story ({ story = '' }): JSX.Element {
+export default function Story ({ component = 'avatar' }): JSX.Element {
   return (<iframe
-    title="Excalibur Storybook Example"
-    src={`https://arc.arup.com/?path=/story/components-arc${story}--default&nav=0`}
+    src={`https://arc.arup.com/iframe.html?instrument=false&viewMode=story&id=components-arc${component}--default`}
     width="100%"
-    height="600"
     style={FRAME_STYLE}
   ></iframe>
   )

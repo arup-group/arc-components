33ca8618973403ec569ea96ad513d5423856d9b5
Dominic Egginton <dom.egginton@arup.com>
2024-09-02 18:06:50 +0100

    feat(alert): arc container alerts bata feature

--- packages/components/src/components/container/ArcContainer.ts
+++ packages/components/src/components/container/ArcContainer.ts
@@ -1,4 +1,4 @@
-import { html, LitElement } from 'lit';
+import { html, LitElement, css } from 'lit';
 import { property, query } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { when } from 'lit/directives/when.js';
@@ -12,6 +12,186 @@ import styles from './arc-container.styles.js';
 import '../navbar/arc-navbar.js';
 import '../accessibility/arc-accessibility.js';
 import '../bottombar/arc-bottombar.js';
+
+import { customElement } from 'lit/decorators.js';
+import componentStyles from '../../styles/component.styles.js';
+
+import {
+  NOTIFICATION_COLORS,
+  NotificationColor,
+} from '../../internal/constants/styleConstants.js';
+import { ARC_EVENTS } from '../../internal/constants/eventConstants.js';
+import { emit } from '../../internal/event.js';
+
+import '../ph-icon/warning/ph-icon-warning.js';
+import '../ph-icon/warning-octagon/ph-icon-warning-octagon.js';
+import '../ph-icon/info/ph-icon-info.js';
+import '../ph-icon/check-circle/ph-icon-check-circle.js';
+import '../ph-icon/x/ph-icon-x.js';
+
+export type NotificationType = NotificationColor;
+
+/**
+ * @ bata component feature (api subject to change)
+ * @internal
+ * @ssr - True
+ */
+@customElement('arc-alert')
+export class ArcAlert extends LitElement {
+  /** @internal */
+  static tag = 'arc-alert';
+
+  /** @internal */
+  static styles = [
+    componentStyles,
+    css`
+      :host {
+        display: block;
+        top: 0;
+        left: 0;
+        width: 100%;
+        z-index: calc(var(--arc-z-index-drawer) - 1);
+        font-size: var(--arc-font-size-small);
+      }
+      span.title {
+        font-weight: bold;
+      }
+      div.alert {
+        display: grid;
+        grid-template-columns: auto 1fr;
+        gap: var(--arc-spacing-medium);
+        padding: var(--arc-spacing-medium);
+        background: rgb(var(--arc-container-color));
+        border-radius: var(--arc-border-radius-small);
+        min-width: 300px;
+        max-width: 500px;
+      }
+      div.details {
+        display: grid;
+        gap: 5px;
+      }
+      arc-icon-button {
+        position: absolute;
+        top: 4px;
+        right: 4px;
+      }
+      div.actions {
+        grid-column: span 2;
+        display: grid;
+        grid-auto-flow: column;
+        gap: var(--arc-spacing-small);
+        justify-content: flex-end;
+      }
+    `,
+  ];
+
+  /** Notification title */
+  @property({ type: String, reflect: true }) title: string;
+
+  /** Notification message */
+  @property({ type: String, reflect: true }) message: string;
+
+  /** Notification type */
+  @property({ type: String, reflect: true }) type: NotificationColor =
+    NOTIFICATION_COLORS.info;
+
+  /** If the notification is dismissable */
+  @property({ type: Boolean }) dismissable = true;
+
+  /** @internal */
+  /** Returns the icon based on the notification type */
+  private icon() {
+    switch (this.type) {
+      case NOTIFICATION_COLORS.error:
+        return html`<ph-icon-warning size="x-large" />`;
+      case NOTIFICATION_COLORS.warning:
+        return html`<ph-icon-warning-octagon size="x-large" />`;
+      case NOTIFICATION_COLORS.info:
+        return html`<ph-icon-info size="x-large" />`;
+      case NOTIFICATION_COLORS.success:
+        return html`<ph-icon-check-circle size="x-large" />`;
+      default:
+        return html`<ph-icon-info size="x-large" />`;
+    }
+  }
+
+  /** @internal */
+  /** Handles the close button click */
+  private handleCloseBtnClick() {
+    emit(this, ARC_EVENTS.hide);
+  }
+
+  /** @internal */
+  /** Action button configuration */
+  @property({ type: Object }) action: ActionConfig;
+
+  /** @internal */
+  /** Secondary action button configuration */
+  @property({ type: Object }) secondaryAction: ActionConfig;
+
+  protected render() {
+    return html`<div
+      class=${classMap({
+        alert: true,
+        'alert--error': this.type === NOTIFICATION_COLORS.error,
+        'alert--warning': this.type === NOTIFICATION_COLORS.warning,
+        'alert--info': this.type === NOTIFICATION_COLORS.info,
+        'alert--success': this.type === NOTIFICATION_COLORS.success,
+      })}
+    >
+      ${this.icon()}
+      <div class="details">
+        <span class="title">${this.title}</span>
+        <span>${this.message}</span>
+      </div>
+
+      <div class="actions">
+        ${when(
+          () => this.secondaryAction,
+          () => html`
+            <arc-button color="Secondary" @click=${this.secondaryAction.action}>
+              ${this.secondaryAction.label}
+            </arc-button>
+          `,
+        )}
+        ${when(
+          () => this.action,
+          () => html`
+            <arc-button color="primary" @click=${this.action.action}>
+              ${this.action.label}
+            </arc-button>
+          `,
+        )}
+      </div>
+
+      ${when(
+        () => this.dismissable,
+        () => html`
+          <arc-icon-button @click=${this.handleCloseBtnClick}>
+            <ph-icon-x slot="icon"></ph-icon-x>
+          </arc-icon-button>
+        `,
+      )}
+    </div>`;
+  }
+}
+
+type Action = () => void;
+interface ActionConfig {
+  label: string;
+  action: Action;
+}
+
+interface AlertConfig {
+  type: 'success' | 'error' | 'warning' | 'info';
+  title: string;
+  message: string;
+  action?: ActionConfig;
+  secondaryAction?: ActionConfig;
+  dismissable?: boolean;
+}
+
+type AlertOrConfig = AlertConfig | ArcAlert;
 
 /**
  * @slot default - The container's content.
@@ -27,6 +207,42 @@ import '../bottombar/arc-bottombar.js';
  * @ssr - True
  */
 export default class ArcContainer extends LitElement {
+  @property({ type: String }) title = '';
+
+  openAlert(config: AlertOrConfig): ArcAlert {
+    const overylay = document.createElement('div');
+    overylay.style.position = 'fixed';
+    overylay.style.top = '0';
+    overylay.style.left = '0';
+    overylay.style.width = '100%';
+    overylay.style.height = '100%';
+    overylay.style.zIndex = '1000';
+    overylay.style.display = 'grid';
+    overylay.style.justifyContent = 'center';
+    overylay.style.alignItems = 'center';
+    overylay.style.backgroundColor = 'rgba(var(--arc-grey-050), 0.5)';
+    if (config instanceof ArcAlert) {
+      overylay.appendChild(config);
+      this.appendChild(overylay);
+      return config;
+    } else {
+      const alert = document.createElement('arc-alert') as ArcAlert;
+      alert.title = config.title;
+      alert.message = config.message;
+      alert.type = config.type;
+      alert.dismissable = config.dismissable ?? true;
+      alert.action = config.action!;
+      alert.secondaryAction = config.secondaryAction!;
+
+      overylay.appendChild(alert);
+      alert.addEventListener('arc-hide', () => {
+        overylay.remove();
+      });
+      this.appendChild(overylay);
+      return alert;
+    }
+  }
+
   /** @internal */
   static tag = 'arc-container';
 

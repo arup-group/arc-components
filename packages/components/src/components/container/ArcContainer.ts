8cae2bf5e6254622e07ea2e3dac35b9d2f11ce52
Dom Egginton <dominic.egginton@gmail.com>
2024-09-12 15:24:26 +0100

    fix(components/container): listen to the arc-accessibility-open event on the bottom solloted element (#298)
    Adds an event listen for the `arc-accessibility-open` event for the
    element in the **bottom** slot of the `ArcContainer` component. The
    event listener opens the accessibility pannel.

--- packages/components/src/components/container/ArcContainer.ts
+++ packages/components/src/components/container/ArcContainer.ts
@@ -105,15 +105,10 @@ export default class ArcContainer extends LitElement {
     return html`
       ${when(this.banner, () => banner)}
       <div id="main">
-        <slot
-          id="nav"
-          name="nav"
-          @arc-show-accessibility=${this.showAccessibility}
-        >
-          <arc-navbar
-            @arc-show-accessibility=${this.showAccessibility}
-          ></arc-navbar>
+        <slot name="nav" @arc-show-accessibility=${this.showAccessibility}>
+          <arc-navbar @arc-show-accessibility=${this.showAccessibility} />
         </slot>
+
         <div
           id="container"
           class=${classMap({
@@ -126,6 +121,7 @@ export default class ArcContainer extends LitElement {
             <slot></slot>
           </div>
         </div>
+
         <slot
           name="accessibility"
           @arc-accessibility-change=${this.handleAccessibilityChange}
@@ -133,12 +129,11 @@ export default class ArcContainer extends LitElement {
           <arc-accessibility
             id="accessibility"
             @arc-accessibility-change=${this.handleAccessibilityChange}
-          ></arc-accessibility>
+          />
         </slot>
-        <slot name="bottom">
-          <arc-bottombar
-            @arc-show-accessibility=${this.showAccessibility}
-          ></arc-bottombar>
+
+        <slot name="bottom" @arc-show-accessibility=${this.showAccessibility}>
+          <arc-bottombar @arc-show-accessibility=${this.showAccessibility} />
         </slot>
       </div>
     `;

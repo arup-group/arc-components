712c54ac654cea5b67a04a7edbb0a8553a088468
Dominic Egginton <dom.egginton@arup.com>
2024-09-19 15:09:29 +0100

    chore(playgrounds): tidy up playgrounds

--- playgrounds/vue/vite.config.ts
+++ playgrounds/vue/vite.config.ts
@@ -1,31 +1,17 @@
 import { defineConfig } from 'vite';
-
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import vue from '@vitejs/plugin-vue';
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/playgrounds/vue',
-
   server: {
     port: 4200,
     host: 'localhost',
   },
-
   plugins: [
     viteTsConfigPaths({
       root: '../../',
     }),
-
     vue(),
-
-    viteStaticCopy({
-      targets: [
-        {
-          src: '../../assets',
-          dest: '',
-        },
-      ],
-    }),
   ],
 });

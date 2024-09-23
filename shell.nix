6a8cac75a3c38c5ba153a17b31c009c3ac0e0b4e
Dom Egginton <dominic.egginton@gmail.com>
2024-09-23 18:15:20 +0100

    chore(workspace): add shell hook for configuring the nodejs js (#318)

--- shell.nix
+++ shell.nix
@@ -1,9 +1,18 @@
-{ pkgs, mkShell, prefetch-npm-deps }:
+{ pkgs, mkShell, nodejs, prefetch-npm-deps, makeSetupHook, writeScript }:
+
+let
+
+  nodejsShellHook = makeSetupHook
+    { name = "nodejs-shell-hook"; substitutions = { npm = "${nodejs}/bin/npm"; }; }
+    (writeScript "nodejs-shell-hook.sh" ''
+      export PATH=$PATH:$(@npm@ bin)
+      export PATH=$PATH:./node_modules/.bin
+    '');
+
+in
 
 mkShell {
   inputsFrom = with pkgs; [ components ];
   packages = with pkgs; [ opentofu azure-cli prefetch-npm-deps ];
-  shellHook = ''
-    export PATH=$PATH:$(npm bin)
-  '';
+  buildInputs = [ nodejsShellHook ];
 }

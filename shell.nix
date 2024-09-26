bde061b73f198193e28ea0b27a49dc453a0dd8a0
Dominic Egginton <dom.egginton@arup.com>
2024-09-26 10:14:57 +0100

    chore(workspace): add sbom

--- shell.nix
+++ shell.nix
@@ -1,15 +1,19 @@
 { pkgs, lib, mkShell, nodejs, prefetch-npm-deps, makeSetupHook, writeScript }:
 
 let
-
   nodejsShellHook = makeSetupHook
     { name = "nodejs-shell-hook"; }
     (writeScript "nodejs-shell-hook.sh" ''export PATH=$PATH:./node_modules/.bin'');
-
 in
 
 mkShell {
-  inputsFrom = with pkgs; [ components ];
-  packages = with pkgs; [ opentofu azure-cli prefetch-npm-deps lib.setup-npm-config ];
   buildInputs = [ nodejsShellHook ];
+  inputsFrom = with pkgs; [ components react ];
+  packages = with pkgs; [
+    opentofu
+    azure-cli
+    prefetch-npm-deps
+    lib.setup-npm-config
+    lib.generate-sbom
+  ];
 }
